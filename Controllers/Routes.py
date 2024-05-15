9a5564afa485c6af74f32ffb5379a3b2a086e730
TungstenVn <40385888+AngelliaX@users.noreply.github.com>
2024-05-15 14:20:51 +0700

    ver1

--- Controllers/Routes.py
+++ Controllers/Routes.py
@@ -1,11 +1,41 @@
-from flask import Blueprint, render_template, make_response, redirect
+from flask import Blueprint, render_template, make_response, redirect, request, url_for
 
 mod = Blueprint('routes', __name__)
 
 
+todos = [{"task":"Sample Todo 1", "done":False},{"task":"Sample Todo 2", "done":False}]
+
+
+
 @mod.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", todos=todos)
+
+@mod.route('/add', methods=["POST"])
+def add():
+    todo = request.form['todo']
+    todos.append({"task": todo, "done": False})
+    return redirect(url_for("routes.index"))
+
+@mod.route('/edit/<int:index>', methods=["GET", "POST"])
+def edit(index):
+    todo = todos[index]
+    if request.method == "POST":
+        todo['task'] = request.form["todo"]
+        return redirect(url_for("routes.index"))
+    else:
+        return render_template("edit.html",todo = todo, index=index)
+
+@mod.route('/check/<int:index>')
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for("routes.index"))
+
+@mod.route('/delete/<int:index>')
+def delete(index):
+    del todos[index]
+    return redirect(url_for("routes.index"))
+
 
 @mod.route('/page2')
 def page2():
